49442463bc197a80842b6ce37a02e8d4e7221667
Andrew Rossignol <andrew@andrew-surfer.(none)>
2012-09-12 20:22:44 -0400

    Started work on the firmware

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,33 @@
 #include <avr/io.h>
+#include <avr/delay.h>
+#include <bitop.h>
 
 void main() {
+	// Set the LEDs as outputs
+	DDRD = 0xF0;
+
+	// Latch power
+	DDRB = 0x02;
+	PORTB = 0x02;
 	
+	DDRC = 0x00;
+	PORTC = 0b00111111;
+	
+	// Wait until the button is not pressed, prevents premature shutdown
+	PORTD = 0x70;
+	while(!(PINC & 0b00000001));
+	
+	while(1) {
+		// Always keep the power LED on
+		PORTD |= 0x70;
+
+		if(!(PINC & 0b00000001)) {
+			_delay_ms(50);
+			if(!(PINC & 0b00000001)) {
+				PORTB = 0x00;
+			}
+		} else {
+			PORTD |= (PINC & 0b00110000) + (0b11 << 6) & (PINC << 4);
+		}
+	}
 }
